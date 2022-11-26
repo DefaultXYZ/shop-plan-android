64c055eab3fdf4fb8d0afabf92e63fcc130c5ff6
Yevhenii Ishchenko <yevhenii.ishchenko@gmail.com>
2022-11-26 20:13:29 +0100

    Update Typography using Material

---
+++ app/src/main/kotlin/xyz/shop/plan/ui/theme/Size.kt
@@ -0,0 +1,128 @@
+package xyz.shop.plan.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object FontSize {
+    /**
+     * Font 22 sp
+     */
+    val ExtraLarge: TextUnit
+        get() = 22.sp
+
+    /**
+     * Font 16 sp
+     */
+    val Large: TextUnit
+        get() = 16.sp
+
+    /**
+     * Font 14 sp
+     */
+    val Medium: TextUnit
+        get() = 14.sp
+
+    /**
+     * Font 12 sp
+     */
+    val Small: TextUnit
+        get() = 12.sp
+
+    /**
+     * Font 11 sp
+     */
+    val ExtraSmall: TextUnit
+        get() = 11.sp
+}
+
+object FontLineHeight {
+    /**
+     * Line height 28 sp
+     */
+    val ExtraLarge: TextUnit
+        get() = 28.sp
+
+    /**
+     * Line height 24 sp
+     */
+    val Large: TextUnit
+        get() = 24.sp
+
+    /**
+     * Line height 20 sp
+     */
+    val Medium: TextUnit
+        get() = 20.sp
+
+    /**
+     * Line height 16 sp
+     */
+    val Small: TextUnit
+        get() = 16.sp
+}
+
+object Size {
+    /**
+     * Size 28 dp
+     */
+    val ExtraLarge: Dp
+        get() = 28.dp
+
+    /**
+     * Size 16 dp
+     */
+    val Large: Dp
+        get() = 16.dp
+
+    /**
+     * Size 12 dp
+     */
+    val Medium: Dp
+        get() = 12.dp
+
+    /**
+     * Size 8 dp
+     */
+    val Small: Dp
+        get() = 8.dp
+
+    /**
+     * Size 4 dp
+     */
+    val ExtraSmall: Dp
+        get() = 4.dp
+}
+
+object PaddingSize {
+    /**
+     * Size 22 dp
+     */
+    val ExtraLarge: Dp
+        get() = 22.dp
+
+    /**
+     * Size 16 dp
+     */
+    val Large: Dp
+        get() = 16.dp
+
+    /**
+     * Size 12 dp
+     */
+    val Medium: Dp
+        get() = 12.dp
+
+    /**
+     * Size 8 dp
+     */
+    val Small: Dp
+        get() = 8.dp
+
+    /**
+     * Size 4 dp
+     */
+    val ExtraSmall: Dp
+        get() = 4.dp
+}
--- app/src/main/kotlin/xyz/shop/plan/ui/theme/Type.kt
+++ app/src/main/kotlin/xyz/shop/plan/ui/theme/Type.kt
@@ -1,28 +1,137 @@
 package xyz.shop.plan.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.em
+import xyz.shop.plan.extensions.PreviewContainer
 
-// Set of Material typography styles to start with
+object TypographyStyle {
+    val TitleLarge: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = FontSize.ExtraLarge,
+            lineHeight = FontLineHeight.ExtraLarge
+        )
+    val TitleMedium: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Medium,
+            fontSize = FontSize.Large,
+            lineHeight = FontLineHeight.Large,
+            letterSpacing = 0.15.em
+        )
+    val TitleSmall: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Medium,
+            fontSize = FontSize.Medium,
+            lineHeight = FontLineHeight.Medium,
+            letterSpacing = 0.01.em
+        )
+
+    val BodyLarge: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = FontSize.Large,
+            lineHeight = FontLineHeight.Large,
+            letterSpacing = 0.15.em
+        )
+    val BodyMedium: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = FontSize.Medium,
+            lineHeight = FontLineHeight.Medium,
+            letterSpacing = 0.25.em
+        )
+    val BodySmall: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = FontSize.Small,
+            lineHeight = FontLineHeight.Small,
+            letterSpacing = 0.04.em
+        )
+
+    val LabelLarge: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Medium,
+            fontSize = FontSize.Medium,
+            lineHeight = FontLineHeight.Medium,
+            letterSpacing = 0.01.em
+        )
+    val LabelMedium: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Medium,
+            fontSize = FontSize.Small,
+            lineHeight = FontLineHeight.Small,
+            letterSpacing = 0.05.em
+        )
+    val LabelSmall: TextStyle
+        get() = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = FontSize.ExtraSmall,
+            lineHeight = FontLineHeight.Small,
+            letterSpacing = 0.05.em
+        )
+}
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    h1 = TypographyStyle.TitleLarge,
+    h2 = TypographyStyle.TitleLarge,
+    h3 = TypographyStyle.TitleMedium,
+    h4 = TypographyStyle.TitleMedium,
+    h5 = TypographyStyle.TitleSmall,
+    h6 = TypographyStyle.TitleSmall,
+    body1 = TypographyStyle.BodyLarge,
+    body2 = TypographyStyle.BodyMedium,
+    subtitle1 = TypographyStyle.BodySmall,
+    subtitle2 = TypographyStyle.BodySmall,
+    button = TypographyStyle.LabelLarge,
+    caption = TypographyStyle.LabelMedium,
+    overline = TypographyStyle.LabelSmall
 )
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewType() {
+    val styles = mapOf(
+        "Title Large" to TypographyStyle.TitleLarge,
+        "Title Medium" to TypographyStyle.TitleMedium,
+        "Title Small" to TypographyStyle.TitleSmall,
+        "Body Large" to TypographyStyle.BodyLarge,
+        "Body Medium" to TypographyStyle.BodyMedium,
+        "Body Small" to TypographyStyle.BodySmall,
+        "Label Large" to TypographyStyle.LabelLarge,
+        "Label Medium" to TypographyStyle.LabelMedium,
+        "Label Small" to TypographyStyle.LabelSmall,
+    )
+
+    PreviewContainer {
+        Surface {
+            Column {
+                styles.map {
+                    Text(
+                        it.key,
+                        style = it.value,
+                        modifier = Modifier.padding(PaddingSize.Medium)
+                    )
+                }
+            }
+        }
+    }
+}
